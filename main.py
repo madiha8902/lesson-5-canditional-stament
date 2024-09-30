078e97f0dc1ed29eea03972afed7ea899a3a9459
madiha8902 <madiha.batool@bellfield.nsw.edu.au>
2024-09-30 19:12:29 +1000

    codingitial statement

---
+++ main.py
@@ -0,0 +1,12 @@
+numbers=int(input("enter to check even or odd"))
+if numbers%2==0:
+    print("even number")
+else:
+    print("odd number")
+
+
+ages=int(input("enter to check eligibility of user"))
+if ages>18:
+    print("allowed")
+else:
+    print("not allowed")
